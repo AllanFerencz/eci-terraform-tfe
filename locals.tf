8b7e0b6250b3840d87b35762fa774ba1845856ec
Allan Ferencz <allanferenczmail@gmail.com>
2024-07-09 16:31:51 -0400

    feat: added workspaces (#1)
    * feat: added workspaces
    
    * feat: fixed variables

--- locals.tf
+++ locals.tf
@@ -17,5 +17,61 @@ locals {
       project_id          = module.project["eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/eci-terraform-github"
     }
+
+    "eci-aws-network" = {
+      description         = "Automation for AWS network resources."
+      execution_mode      = "remote"
+      project_id          = module.project["eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/eci-terraform-aws-network"
+
+      variables = [
+        {
+          category = "terraform"
+          hcl      = true
+          key      = "azs"
+          value    = jsonencode(["us-west-2a", "us-west-2b"])
+        },
+        {
+          category = "terraform"
+          key      = "cidr"
+          value    = "10.0.0.0/16"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "eci"
+        },
+      ]
+    }
+
+    "eci-aws-cluster-prod" = {
+      description         = "Automation for AWS cluster resources."
+      execution_mode      = "remote"
+      project_id          = module.project["eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/eci-terraform-aws-cluster"
+
+      variables = [
+        {
+          category = "terraform"
+          key      = "domain"
+          value    = "devaf.dev"
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "eci-allanferencz"
+        },
+        {
+          category = "terraform"
+          key      = "vpc_name"
+          value    = "eci"
+        },
+      ]
+    }
   }
 }
